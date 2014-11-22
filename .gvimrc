33c3835e795c4ff80b5344585a260147c7704cbe
ashyktj <ash.yktj@gmail.com>
2014-11-22 16:45:15 +0900

    remove undofile and add gvimrc

---
+++ .gvimrc
@@ -0,0 +1,11 @@
+" GUI {{{
+set transparency=10
+set lines=48
+set columns=100
+set noimdisableactivate
+set imdisable
+set undodir=~/.undodir
+colorscheme molokai
+syntax on
+
+" "}}}
--- link.sh
+++ link.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 ln -s ~/Documents/Git/config/.vimrc ~/.vimrc
+ln -s ~/Documents/Git/config/.gvimrc ~/.gvimrc
 ln -s ~/Documents/Git/config/.vimshrc ~/.vimshrc
 ln -s ~/Documents/Git/config/.vrapperrc ~/.vrapperrc
 ln -s ~/Documents/Git/config/.bash_profile ~/.bash_profile
