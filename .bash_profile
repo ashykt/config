42ba4d7e2f50cb4b54a7a624bc5da96680a40d47
ashyktj <ash.yktj@gmail.com>
2014-11-22 16:24:37 +0900

    initial

---
+++ .bash_profile
@@ -0,0 +1,6 @@
+alias ll="ls -la"
+export EDITOR=/Applications/MacVim.app/Contents/MacOS/mvim
+alias vi='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/mvim "$@"'
+alias vim='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/mvim "$@"'
+
+
---
+++ link.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+ln -s ~/Documents/Git/config/.vimrc ~/.vimrc
+ln -s ~/Documents/Git/config/.vimshrc ~/.vimshrc
+ln -s ~/Documents/Git/config/.vrapperrc ~/.vrapperrc
+ln -s ~/Documents/Git/config/.bash_profile ~/.bash_profile
+ln -s ~/Documents/Git/config/.bashrc ~/.bashrc
+
+
